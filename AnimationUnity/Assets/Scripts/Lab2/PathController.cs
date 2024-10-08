2baea3599937e766ecdeba45abd892bf9eb7aa59
tomNguyenOTU <114022123+tomNguyenOTU@users.noreply.github.com>
2024-10-07 23:46:32 -0400

    lab3 wip

---
+++ AnimationUnity/Assets/Scripts/Lab2/PathController.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathController : MonoBehaviour
+{
+    [SerializeField]
+    public PathManager pathManager;
+
+    List<Waypoint> thePath;
+    Waypoint target;
+
+    public float MoveSpeed;
+    public float RotateSpeed;
+
+    public Animator animator;
+    bool isWalking;
+
+    void RotateTowardsTarget() {
+        float stepSize = RotateSpeed * Time.deltaTime;
+
+        Vector3 targetDir = target.pos - transform.position;
+        Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, stepSize, 0.0f);
+        transform.rotation = Quaternion.LookRotation(newDir);
+    }
+    void MoveForward() {
+        float stepSize = MoveSpeed * Time.deltaTime;
+        float distanceToTarget = Vector3.Distance(transform.position, target.pos);
+        
+        if (distanceToTarget < stepSize) {
+            // overshooting target
+            return;
+        }
+
+        Vector3 moveDir = Vector3.forward;
+        transform.Translate(moveDir * stepSize);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        thePath = pathManager.GetPath();
+        if (thePath != null && thePath.Count > 0) {
+            target = thePath[0];
+        }
+
+        isWalking = true;
+        animator.SetBool("isWalking", isWalking);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            // toggle if key pressed
+            isWalking = !isWalking;
+            animator.SetBool("isWalking", isWalking);
+        }
+        if (isWalking) {
+            RotateTowardsTarget();
+            MoveForward();
+        }
+        //if (target == null) {
+        //    target = thePath[0];
+        //}
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        // next target
+        target = pathManager.GetNextTarget();
+    }
+}
---
+++ AnimationUnity/Assets/Scripts/Lab2/PathManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathManager : MonoBehaviour
+{
+    [HideInInspector]
+    [SerializeField]
+    public List<Waypoint> path;
+    public GameObject prefab;
+    int currentPointIndex = 0;
+    public List<GameObject> prefabPoints;
+
+    public List<Waypoint> GetPath() {
+        if (path == null)
+            path = new List<Waypoint>();
+
+        return path;
+    }
+
+    public void CreateAddPoint() {
+        Waypoint go = new Waypoint();
+        path.Add(go);
+    }
+
+    public Waypoint GetNextTarget() {
+        int nextPointIndex = (currentPointIndex + 1) % (path.Count);
+        currentPointIndex = nextPointIndex;
+        return path[nextPointIndex];
+    }
+
+    public void Start() {
+        // prefabPoints = new List<GameObject>();
+        // create prefab colliders
+        foreach (Waypoint p in path) {
+            GameObject go = Instantiate(prefab);
+            go.transform.position = p.pos;
+            prefabPoints.Add(go);
+        }
+    }
+
+    public void Update() {
+        // update prefabs to waypoints
+        for (int i = 0; i < path.Count; i++) {
+            Waypoint p = path[i];
+            GameObject g = prefabPoints[i];
+            g.transform.position = p.pos;
+        }
+    }
+}
